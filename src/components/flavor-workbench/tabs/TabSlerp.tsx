fcf7e94cbaa94fc1304d9bc0c20572f3f750f260
BotonJ <2628583513@qq.com>
2026-06-08 13:10:00 +0800

    chore: 移除未使用的 useEffect 导入

--- src/components/flavor-workbench/tabs/TabSlerp.tsx
+++ src/components/flavor-workbench/tabs/TabSlerp.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo, useCallback, useEffect } from 'react';
+import { useState, useMemo, useCallback } from 'react';
 import { motion } from 'framer-motion';
 import { getIngredients, getCooccurrencePairs, CATEGORY_COLORS } from '../data/ingredients';
 import { slerp2d, nearestK, arcPath, type Vec2 } from '../lib/slerp';
